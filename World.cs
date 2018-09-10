ab922b89fd7d98076a1af8187c1a32e7a0af932d
wolfjosiah <39775622+wolfjosiah@users.noreply.github.com>
2018-09-09 20:24:16 -0700

    World area (#6)
    * Area procedure, returns an area in world.
    
    * Private constructor for use with Area procedure.
    
    * Filler not needed with fillerless constructor.
    
    * Refactored Area Procedure

--- World.cs
+++ World.cs
@@ -8,13 +8,33 @@ public class World<T>
 		this.world = new T[width, height, depth];
 		this.Fill(filler);
 	}
-	
+  
+	private World (int width, int height, int depth)
+	{
+		this.world = new T[width, height, depth];
+	}
+  
 	private void Fill (T filler)
 	{
 		for (var x = 0; x < Width(); x++)
 		for (var y = 0; y < Height(); y++)
 		for (var z = 0; z < Depth(); z++)
 			Write(x, y, z, filler);
+	}
+	
+	public World<T> Area (int x0, int y0, int z0, int x1, int y1, int z1)
+	{
+		var width  = x1 - x0 + 1;
+		var height = y1 - y0 + 1;
+		var depth  = z1 - z0 + 1;
+		World<T> area = new Area (width, height, depth);
+		
+		for (var x = x0; x <= x1; x++)
+		for (var y = y0; y <= y1; y++)
+		for (var z = z0; z <= z1; z++)
+			area.Write (x, y, z, Read (x, y, z));
+		
+		return area;
 	}
 	
 	// Dimensions
